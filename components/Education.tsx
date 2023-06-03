a450cb1c734fdf2902a973d4578af2e100e81bee
Preyansh Patel <patelpreyansh3011@gmail.com>
2023-06-03 11:42:17 +0530

    experiance section dynamically added

--- components/Education.tsx
+++ components/Education.tsx
@@ -1,26 +1,26 @@
 import React from "react";
-import { educationItems } from "../appData/data";
+import { educationItems, experianceItems } from "../appData/data";
 
 const Education = () => {
   return (
     <div className="education-main" id="education">
       <div className="education-sec">
         <div className="h1-font">Experience</div>
-        <div className="education-sec-item">
-          <p className="h2-font purple-font">ReactJS Developer Intern</p>
-          <p className="h3-bold-font">Oyebusy Pvt. Ltd.</p>
-          <p className="education-item-date">AUGUST-2022, PRESENT</p>
-          <li className="h3-font">
-            During the training period of internship, I learn ReactJs in detail
-            and gain knowledge of other library like Material UI, Redux, Redux
-            Toolkit, etc.
-          </li>
-          <li className="h3-font">
-            In this I worked on Admin Panel of the company and create UI based
-            on Figma, create reuseful components, how to manage state for web
-            app.
-          </li>
-        </div>
+        {experianceItems.map((item, index) => (
+          <div
+            className="education-sec-item"
+            key={`experiance-item-index:${index}`}
+          >
+            <p className="h2-font purple-font">{item.position}</p>
+            <p className="h3-bold-font">{item.company}</p>
+            <p className="education-item-date">{item.duration}</p>
+            {item.learnings.split("\n").map((learningItem, index) => (
+              <li className="h3-font" key={`experiance-learning-item-index:${index}`}>
+                {learningItem}
+              </li>
+            ))}
+          </div>
+        ))}
       </div>
       <div className="education-sec">
         <div className="h1-font">Education</div>
