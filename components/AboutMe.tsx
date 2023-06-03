22f2502a35e25d9016037b5092472d58104f4b46
Preyansh Patel <patelpreyansh3011@gmail.com>
2023-06-03 11:30:14 +0530

    about me paragraph updated

--- components/AboutMe.tsx
+++ components/AboutMe.tsx
@@ -5,7 +5,11 @@ const AboutMe = () => {
   return (
     <div id="about" className="aboutme-main">
       <p className="h1-font">About Me</p>
-      <p>{`I am Preyansh Patel from Ahmedabad, Gujarat and I have to pursue my b.tech degree from a Silver Oak University, Ahmedabad in the branch of Computer Engineering with Ai/Ml`}</p>
+      <p>{`Hello! I'm a passionate computer engineering student at Silver Oak University, located in the vibrant city of Ahmedabad, Gujarat.`}</p>
+      <p>{`I am deeply fascinated by the world of web development, particularly ReactJS and Python.`}</p>
+      <p>{`I love creating immersive and user-friendly web experiences, and I constantly seek to expand my knowledge and skills in these areas.`}</p>
+      <p>{`With a creative mindset and a strong attention to detail, I strive to craft visually appealing and robust applications that provide seamless user interactions.`}</p>
+      <p>{`I am eager to contribute to the ever-evolving field of web development and make a positive impact through my work. Let's build amazing things together!`}</p>
       <div
         style={{
           marginTop: "10px",
